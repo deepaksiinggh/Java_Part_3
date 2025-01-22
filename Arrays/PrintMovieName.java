4d19a1f33cc7dcfbb9d5fa02835a59825dcad2ce
Deepak singh <130153943+sdeepak83@users.noreply.github.com>
2025-01-22 19:26:36 +0530

    ArraySimpleQuestion

---
+++ Arrays/PrintMovieName.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+public class PrintMovieName {
+    public static void main(String[] args) {
+        String[] movieName = { "Avatar", "Intrestaller", "Life", "Gravity", "Life of pie", "Cast Away", "NoteBook",
+                "Indiana Zones" };
+
+        for (int i = 0; i < movieName.length; i++) {
+            System.out.println(movieName[i]);
+        }
+    }
+
+}
